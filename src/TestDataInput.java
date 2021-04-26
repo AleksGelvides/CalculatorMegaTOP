44986e3b197cecb3cdfe62f24aa2bae2d64b1279
AGCPA <60176859+AGCPA@users.noreply.github.com>
2021-04-26 22:50:38 +0700

    1

--- src/TestDataInput.java
+++ src/TestDataInput.java
@@ -2,7 +2,5 @@ public class TestDataInput {
     static char operator;
     public static void operator () {
         operator = 's';
-        int i = 5+5;
-        System.out.println(operator + i);
     }
 }
