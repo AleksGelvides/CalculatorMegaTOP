81c457b3366667600a180d4ed7e32c8e648af789
AGCPA <60176859+AGCPA@users.noreply.github.com>
2021-04-26 22:47:47 +0700

    1

---
+++ src/TestDataInput.java
@@ -0,0 +1,8 @@
+public class TestDataInput {
+    static char operator;
+    public static void operator () {
+        operator = 's';
+        int i = 5+5;
+        System.out.println(operator + i);
+    }
+}
