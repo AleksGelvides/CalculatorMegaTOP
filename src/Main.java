f47dc7bce0f3c3d51f4b9d3146e0a478ac91cbf6
AGCPA <60176859+AGCPA@users.noreply.github.com>
2021-04-26 22:47:47 +0700

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        
+        boolean workProcess = true;
+        boolean calculationProcess = true;
+        while (workProcess == true) {
+
+//            char sysOff = 's';
+//                while (calculationProcess == true){
+//
+//                }
+            if (TestDataInput.operator == 's') {
+            workProcess = true;
+            }
+
+            System.out.println("System: " + workProcess);
+        }
     }
 }
