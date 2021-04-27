3e8bed7893f31c20466c2289f7939751b4746b27
Пользователь <gelvides.aleksey@outloo.com>
2021-04-27 17:15:07 +0700

    re.main

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         boolean workProcess = true;
-        boolean calculationProcess = true;
+        boolean calculationProcess;
         while (workProcess == true) {
+            calculationProcess = true;
             while (calculationProcess == true){
                     TestDataInput.inOperator();
                 switch (TestDataInput.operator) {
-                        case 'c':
-                            calculationProcess = false;
-                            break;
                         case 's':
                             workProcess = false;
                             break;
                 }
-                if (calculationProcess == false){
-                    System.out.flush();}
-                if (workProcess  == false) {
-                    break;
-                }
+                if (TestDataInput.operator == 'c'){
+                    System.out.print("\033[H\033[2J");
+                    System.out.flush();
+                    calculationProcess = false;}
+                if (!workProcess) {
+                    break; }
+                System.out.println("Вы ввели " + TestDataInput.operator);
             }
         }
     }
